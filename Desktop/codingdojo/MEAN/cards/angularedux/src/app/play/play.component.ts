c246fea62a556d633400305bc89415444c510d4a
WDMJudge <willdmj@gmail.com>
2018-08-21 13:18:19 -0700

    added most of the wireframe

--- Desktop/codingdojo/MEAN/cards/angularedux/src/app/play/play.component.ts
+++ Desktop/codingdojo/MEAN/cards/angularedux/src/app/play/play.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent implements OnInit {
-
+  switch: boolean = false;
+  switch2: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+
+
+
+
+
+  cardVal() {
+    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+}
+
+// INCLUSIVE
+// getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 }
