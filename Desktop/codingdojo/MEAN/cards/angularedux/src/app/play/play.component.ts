bafa8968c7a24a44efcf9aa9f4b0e3d6cb7822c1
WDMJudge <willdmj@gmail.com>
2018-08-21 15:24:04 -0700

    added random card position and value on the play component

--- Desktop/codingdojo/MEAN/cards/angularedux/src/app/play/play.component.ts
+++ Desktop/codingdojo/MEAN/cards/angularedux/src/app/play/play.component.ts
@@ -8,20 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class PlayComponent implements OnInit {
   switch: boolean = false;
   switch2: boolean = false;
+  cardOne: any;
+  cardTwo: any;
   constructor() { }
 
   ngOnInit() {
+    this.cardOne = {left: this.cardPosition(), top: this.cardPosition(), val: this.cardVal()};
+    this.cardTwo = {left: this.cardPosition(), top: this.cardPosition(), val: this.cardVal()};
   }
 
-
-
-
-
-
+  cardPosition() {
+    return Math.floor(Math.random() * (80 - 10 + 1)) + 10;
+  }
 
   cardVal() {
     return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-}
+  }
 
 // INCLUSIVE
 // getRandomInt(min, max) {
